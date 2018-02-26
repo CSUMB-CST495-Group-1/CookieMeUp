6a7306cca2397f70a88cbf180b49ddc1bc771ecd
RaquelFigueroa <rfiqueroa@csumb.edu>
2018-02-26 14:32:05 -0800

    Initial Commit

---
+++ CookieMeUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CookieMeUp
+//
+//  Created by Raquel Figueroa-Opperman on 2/26/18.
+//  Copyright © 2018 CSUMB-CST495-Group-1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
