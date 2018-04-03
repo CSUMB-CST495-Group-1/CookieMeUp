2a2e59fdd106005d2e968ee993fd9a89419f3b64
lesley amezcua <lesleyamezcua@lesley-amezcuas-MacBook-Pro.local>
2018-04-02 17:59:37 -0700

    Added Map

--- CookieMeUp/ViewControllers/MapViewController.swift
+++ CookieMeUp/ViewControllers/MapViewController.swift
@@ -7,9 +7,42 @@
 //
 
 import UIKit
-
-class MapViewController: UIViewController {
-
+import MapKit
+class MapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "myAnnotationView"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseID)
+        if (annotationView == nil) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            annotationView!.canShowCallout = true
+            annotationView!.leftCalloutAccessoryView = UIImageView(frame: CGRect(x:0, y:0, width: 50, height:50))
+        }
+        let resizeRenderImageView = UIImageView(frame: CGRect(x:0, y:0, width:45, height:45))
+        resizeRenderImageView.layer.borderColor = UIColor.white.cgColor
+        resizeRenderImageView.layer.borderWidth = 3.0
+        resizeRenderImageView.contentMode = UIViewContentMode.scaleAspectFill
+        
+        UIGraphicsBeginImageContext(resizeRenderImageView.frame.size)
+        resizeRenderImageView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let thumbnail = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        let imageView = annotationView?.leftCalloutAccessoryView as! UIImageView
+        imageView.image = thumbnail
+        
+        let detailButton = UIButton(type: .detailDisclosure)
+        annotationView?.rightCalloutAccessoryView = detailButton
+        
+        annotationView?.image = thumbnail
+        
+        return annotationView
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
